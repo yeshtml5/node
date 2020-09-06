343841b204724c51f049e9dda8d255a6c4423438
wanhwi.son <yeshtml5@gmail.com>
2020-09-06 10:02:03 +0900

    index.html 읽어오기

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import * as express from "express";
+
 class App {
   public application: express.Application;
   constructor() {
     this.application = express();
   }
 }
+
 const app = new App().application;
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("start");
+  res.sendFile(__dirname + "/index.html");
 });
-app.listen(4000, () => console.log("start"));
+app.get("/yeshtml5", (req: express.Request, res: express.Response) => {
+  res.send("yeshtml5");
+});
+app.listen(4000, () => console.log("start1"));
