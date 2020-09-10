c8a25b0042efe8717727a9190aa5d62ddebe9e2b
Noah <yeshtml5@gmail.com>
2020-09-10 20:35:58 +0900

    nodejs 설정파일

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,15 @@
 import * as express from "express";
+import { Application, Request, Response, NextFunction } from "express";
 
-class App {
-  public application: express.Application;
-  constructor() {
-    this.application = express();
-  }
-}
-
-const app = new App().application;
+const app: Application = express();
 
 const add = (a: number, b: number): number => a + b;
-app.get(
-  "/",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(__dirname);
-    res.sendFile(__dirname + "/index.html");
-    console.log(add(4, 3));
-  }
-);
-app.get("/yeshtml5", (req: express.Request, res: express.Response) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
+  console.log(add(4, 3));
+});
+app.get("/yeshtml5", (req: Request, res: Response) => {
   res.send("yeshtml5");
 });
-app.listen(4000, () => console.log("start1"));
+app.listen(5000, () => console.log("start"));
